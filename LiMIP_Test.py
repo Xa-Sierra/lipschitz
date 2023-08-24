5bdee2766027bc6eb0acfcf08f34a8dda43ff527
Xa-Sierra <143114381+Xa-Sierra@users.noreply.github.com>
2023-08-24 10:30:17 -0600

    Update LiMIP_Test.py

--- LiMIP_Test.py
+++ LiMIP_Test.py
@@ -8,25 +8,25 @@ import numpy as np
 import torch.nn as nn 
 import time
 
-# As an example, we'll consider binary classification over R^2
+# load net
 b1=np.load('b1.npy')
 b2=np.load('b2.npy')
 w1=np.load('w1.npy')
 w2=np.load('w2.npy')
 x = np.load('input.npy')
 
-to=np.random.rand(x.shape[0])*3.14  # solo para senoidal vectorial, en senoidal colocar 1
+to=np.random.rand(x.shape[0])*3.14  #use for sin function
 
 
 dim = len(x)
-dimh=np.load('dimo.npy') #colocar el numero de neuronas ocultas de la XOR
+dimh=np.load('dimo.npy') #number of hidden seurons XOR
 
 # Define an input domain and c_vector 
 simple_domain = Hyperbox.build_unit_hypercube(dim)
 simple_c_vector = torch.Tensor([1])
 
 '''
-#modelo de una capa
+#use for one hidden layer model
 model = ReLUNet([dim, dimh, 1]) #
 model.net[0].weight=nn.Parameter(torch.Tensor(w1))
 model.net[0].bias=nn.Parameter(torch.Tensor(b1))
@@ -34,7 +34,7 @@ model.net[2].weight=nn.Parameter(torch.Tensor(w2))
 model.net[2].bias=nn.Parameter(torch.Tensor(b2))
 
 '''
-#modelo de dos capas
+#use for two hidden layers model
 
 b3=np.load('b3.npy')
 w3=np.load('w3.npy')
@@ -55,7 +55,6 @@ simple_result = simple_prob.compute_max_lipschitz()
 
 print(simple_result)  # A lot more information under the hood 
 print('\n')
-#print(w1)
 
 execution_time = (time.time() - start_time)
 print('Execution time in seconds: ' + str(execution_time))
